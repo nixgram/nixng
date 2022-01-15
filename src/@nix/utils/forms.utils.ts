6f2093dc658508e6f2043c9cd439a3486e7c5592
lifeoflikhon <mehedi.hasansjs@gmail.com>
2022-01-15 10:18:38 +0600

    feat: added a function what can touch all the controls of form

--- src/@nix/utils/forms.utils.ts
+++ src/@nix/utils/forms.utils.ts
@@ -1,1 +1,20 @@
-export {};
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+
+export function markFormAsTouched( form: FormGroup, controls: string[] = [], touchNested: boolean ) {
+  if ( controls.length === 0 ) {
+    form.markAllAsTouched();
+    return;
+  }
+
+  controls.forEach( control => {
+    markControlAsTouched( form.get( control ), touchNested );
+  } );
+}
+
+export function markControlAsTouched( control: AbstractControl | FormControl, touchNested: boolean ) {
+  if ( touchNested ) {
+    control.markAllAsTouched();
+  } else {
+    control.markAsTouched();
+  }
+}
