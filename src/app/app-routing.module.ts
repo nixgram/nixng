684f39b584211d48d90e0a5f6d7012b347c9eb16
sefatanam <sefatanam@gmail.com>
2022-11-10 18:27:30 +0600

    route was missing from root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: AppComponent
+  },
+  {
+    path: '**',
+    component: AppComponent
+  }
+];
 
 @NgModule({
-            imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
